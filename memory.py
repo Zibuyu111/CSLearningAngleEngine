3932f7a288f8318507d579ce27b6a8778b6bc353
ziyue <1580001714@qq.com>
2026-06-17 16:09:33 +0800

    feat: reuse register built memory 4096

---
+++ memory.py
@@ -0,0 +1,18 @@
+from register import Register
+
+MAX_MEMORY = 4096
+
+
+class Memory:
+    def __init__(self) -> None:
+        self.cells = [Register() for _ in range(MAX_MEMORY)]
+        # reuse class Register,They have same fundamation
+
+    def read(self, address):
+        if 0 <= address < MAX_MEMORY:
+            return self.cells[address].read()
+
+    def load(self, address, byte_val: int):
+        if 0 <= address < MAX_MEMORY:
+            self.cells[address].load(byte_val)
+            return self.cells[address].read()
