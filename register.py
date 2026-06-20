f5992bd89a4f3f019c515059c32563c347b57bba
ziyue <1580001714@qq.com>
2026-06-20 15:54:40 +0800

    refactor: register from for_in to thread

--- register.py
+++ register.py
@@ -1,4 +1,5 @@
-import re
+from concurrent.futures import ThreadPoolExecutor
+from threading import Barrier, Thread
 
 from bit import Bit
 
@@ -7,6 +8,11 @@ class Register:
     def __init__(self, bit_width=8):
         self.bit_width = bit_width
         self.bits = [Bit() for _ in range(self.bit_width)]
+        self.clk_barrier = Barrier(bit_width)
+
+    def _sync_write_cell(self, idx: int, val: int):
+        self.clk_barrier.wait()
+        self.bits[idx].set() if val else self.bits[idx].reset()
 
     def read(self):
         return [bit.read() for bit in self.bits]
@@ -14,16 +20,16 @@ class Register:
     def load(self, bit_list: list[int]) -> None:
         num = 0
         for idx, bit in enumerate(bit_list):
-            if bit:
-                num |= 1 << idx
+            num |= bit << idx
 
         mask = (1 << self.bit_width) - 1
-        num = num & mask
+        num &= mask
 
-        full = []
-        temp = num
-        for _ in range(self.bit_width):
-            full.append(temp & 1)
-            temp >>= 1
-        for bit_idx in range(self.bit_width):
-            self.bits[bit_idx].set() if full[bit_idx] else self.bits[bit_idx].reset()
+        full = [(num >> i) & 1 for i in range(self.bit_width)]
+        with ThreadPoolExecutor(max_workers=self.bit_width) as pool:
+            tasks = [
+                pool.submit(self._sync_write_cell, idx, val)
+                for idx, val in enumerate(full)
+            ]
+            for task in tasks:
+                task.result()
